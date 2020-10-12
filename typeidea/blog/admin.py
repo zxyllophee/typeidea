8aa7d49029a600ae5b55c45b7c10b5435ba3b7e9
zee <362258867@qq.com>
2020-10-12 11:13:54 +0800

    '初改admin.py，models.py新增__str__'

--- typeidea/blog/admin.py
+++ typeidea/blog/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
+from .models import Category, Post, Tag
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_play = ('name', 'status', 'is_nav', 'created_time')
+    fields = ('name', 'status', 'is_nav')
+
+    def save_model(self, request, obj, form, change):
+        obj.owner = request.user
+        super().save_model(request, obj, form, change)
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'status', 'created_time')
+    fields = ('name', 'status')
+
+    def save_model(self, request, obj, form, change):
+        obj.owner = request.user
+        super().save_model(request, obj, form, change)
+
 # Register your models here.
--- typeidea/blog/models.py
+++ typeidea/blog/models.py
@@ -20,6 +20,9 @@ class Category(models.Model):
     class Meta:
         verbose_name = verbose_name_plural = '分类'
 
+    def __str__(self):
+        return self.name
+
 
 class Tag(models.Model):
     STATUS_NORMAL = 1
@@ -37,6 +40,10 @@ class Tag(models.Model):
 
     class Meta:
         verbose_name = verbose_name_plural = '标签'
+
+
+    def __str__(self):
+        return self.name
 
 
 class Post(models.Model):
@@ -60,5 +67,7 @@ class Post(models.Model):
         verbose_name = verbose_name_plural = '文章'
         ordering = ['-id']
 
+    def __str__(self):
+        return self.title
 
 # Create your models here.
